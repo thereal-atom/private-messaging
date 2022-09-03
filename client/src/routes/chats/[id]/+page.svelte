efe175a8233b881027cf221710a90cd24a30dc2f
thereal-atom <oscarfal2006@gmail.com>
2022-09-03 13:45:49 +0100

    chat - lorem text, lighter message font

--- client/src/routes/chats/[id]/+page.svelte
+++ client/src/routes/chats/[id]/+page.svelte
@@ -37,21 +37,21 @@
             />
         </div>
     </div>
-    <div class="flex-auto flex flex-col-reverse overflow-y-scroll items-start h-full p-4">
-        <div class="message-container right"><div class="message">this is some asda </div></div>
-        <div class="message-container right"><div class="message">msg1</div></div>
-        <div class="message-container right"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container right"><div class="message">this is some asda </div></div>
-        <div class="message-container right"><div class="message">msg1</div></div>
-        <div class="message-container left"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container right"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container left"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container left"><div class="message">msg</div></div>
-        <div class="message-container right"><div class="message">this is some asda </div></div>
-        <div class="message-container right"><div class="message">msg1</div></div>
-        <div class="message-container right"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container left"><div class="message">this is a slightly longer messagge asdjsad asdnsad asd asd sad asd</div></div>
-        <div class="message-container left"><div class="message">msg</div></div>
+    <div class="flex-auto flex flex-col overflow-y-scroll items-start h-full p-4">
+        <div class="message-container right"><div class="message">Lorem ipsum dolor sit</div></div>
+        <div class="message-container right"><div class="message">amet consectetur adipisicing elit. Voluptatum modi quo rerum eveniet</div></div>
+        <div class="message-container right"><div class="message">atque facilis hic, aspernatur iusto nisi! Tempora omnis fugiat ad rerum</div></div>
+        <div class="message-container right"><div class="message">accusamus accusantium</div></div>
+        <div class="message-container right"><div class="message">laboriosam</div></div>
+        <div class="message-container left"><div class="message">recusandae aliquid eligendi.</div></div>
+        <div class="message-container right"><div class="message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae</div></div>
+        <div class="message-container left"><div class="message">veritatis ipsa, sapiente quae tempora expedita, distinctio architecto</div></div>
+        <div class="message-container left"><div class="message">quisquam impedit dicta eum.</div></div>
+        <div class="message-container right"><div class="message">Porro, et!</div></div>
+        <div class="message-container right"><div class="message">Vel</div></div>
+        <div class="message-container right"><div class="message">eius nemo quod fuga aliquam? Maiores?</div></div>
+        <div class="message-container left"><div class="message">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div></div>
+        <div class="message-container left"><div class="message">Lorem</div></div>
     </div>
     <div class="flex flex-row items-center p-4 border-t border-solid border-secondary">
         <img
@@ -73,7 +73,7 @@
 
 <style lang="postcss">
     .message {
-        @apply flex flex-col font-black px-3 py-1 text-sm bg-secondary rounded-md rounded-bl-sm mt-2 w-fit;
+        @apply flex flex-col font-bold px-3 py-1.5 text-sm bg-secondary rounded-md rounded-bl-sm mt-2 w-fit;
     }
     .message-container.left {
         @apply pr-16;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	server: { port: 1337 },
+    plugins: [ sveltekit() ],
+    server: { port: 1337 },
 };
 
 export default config;
